a8683e511914ca081d8dc47cf17cddafd13efbb8
MrDrooxBR <67605518+MrDrooxBR@users.noreply.github.com>
2021-03-24 17:54:29 -0300

    Proposing to remove  String.replace()

--- src/perm.js
+++ src/perm.js
@@ -1,38 +1,34 @@
-module.exports = (PermDefined) => {
-    try {
-        let perm = PermDefined.replace('CREATE_INSTANT_INVITE', 'Criar um Convite')
-        .replace('KICK_MEMBERS', 'Expulsar Membros')
-        .replace('BAN_MEMBERS', 'Banir Membros')
-        .replace('ADMINISTRATOR', 'Administrador')
-        .replace('MANAGE_CHANNELS', 'Gerenciar Canais')
-        .replace('MANAGE_GUILD', 'Gerenciar Servidor')
-        .replace('ADD_REACTIONS', 'Adicionar Reações')
-        .replace('VIEW_AUDIT_LOG', 'Ver o Registro de Auditoria')
-        .replace('PRIORITY_SPEAKER', 'Voz Prioritária')
-        .replace('STREAM', 'Vídeo')
-        .replace('VIEW_CHANNEL', 'Ver Canal')
-        .replace('SEND_MESSAGES', 'Enviar Mensagens')
-        .replace('SEND_TTS_MESSAGES', 'Enviar Mensagens de Voz')
-        .replace('MANAGE_MESSAGES', 'Gerenciar Mensagens')
-        .replace('EMBED_LINKS', 'Inserir Links')
-        .replace('ATTACH_FILES', 'Anexar Arquivos')
-        .replace('READ_MESSAGE_HISTORY', 'Ver Histórico de Mensagens')
-        .replace('MENTION_EVERYONE', 'Mencionar Everyone')
-        .replace('USE_EXTERNAL_EMOJIS', 'Usar Emojis Externos')
-        .replace('VIEW_GUILD_INSIGHTS', 'Ver Análises do Servidor')
-        .replace('CONNECT', 'Conectar')
-        .replace('SPEAK', 'Falar')
-        .replace('MUTE_MEMBERS', 'Silenciar Membros')
-        .replace('DEAFEN_MEMBERS', 'Ensurdecer Membros')
-        .replace('MOVE_MEMBERS', 'Mover Membros')
-        .replace('CHANGE_NICKNAME', 'Alterar Apelido')
-        .replace('MANAGE_NICKNAMES', 'Gerenciar Apelidos')
-        .replace('MANAGE_ROLES', 'Gerenciar Cargos')
-        .replace('MANAGE_WEBHOOKS', 'Gerenciar WebHooks')
-        .replace('MANAGE_EMOJIS', 'Gerenciar Emojis')
-
-        return perm;
-    } catch (err) {
-        return console.log(err)
-    }
+module.exports = (permission) => {
+    return {
+        CREATE_INSTANT_INVITE: "Criar um Convite",
+        KICK_MEMBERS: "Expulsar Membros",
+        BAN_MEMBERS: "Banir Membros",
+        ADMINISTRATOR: "Administrador",
+        MANAGE_CHANNELS: "Gerenciar Canais",
+        MANAGE_GUILD: "Gerenciar Servidor",
+        ADD_REACTIONS: "Adicionar Reações",
+        VIEW_AUDIT_LOGS: "Ver o Registro de Auditoria",
+        PRIORITY_SPEAKER: "Voz Prioritária",
+        STREAM: "Vídeo",
+        VIEW_CHANNEL: "Ver Canal",
+        SEND_MESSAGES: "Enviar Mensagens",
+        SEND_TTS_MESSAGES: "Enviar Mensagens Text to Speech",
+        MANAGE_MESSAGES: "Gerenciar Mensagens",
+        EMBED_LINKS: "Inserir Links",
+        ATTACH_FILES: "Anexar Arquivos",
+        READ_MESSAGE_HISTORY: "Ver o Histórico de Mensagens",
+        MENTION_EVERYONE: "Mencionar Todos",
+        USE_EXTERNAL_EMOJIS: "Usar Emojis Externos",
+        VIEW_GUILD_INSIGHTS: "Ver Análises do Servidor",
+        CONNECT: "Conectar",
+        SPEAK: "Falar",
+        MUTE_MEMBERS: "Silenciar Membros",
+        DEAFEN_MEMBERS: "Ensurdecer Membros",
+        MOVE_MEMBERS: "Mover Membros",
+        CHANGE_NICKNAME: "Alterar Apelido",
+        MANAGE_NICKNAMES: "Gerenciar Apelidos",
+        MANAGE_ROLES: "Gerenciar Cargos",
+        MANAGE_WEBHOOKS: "Gerenciar WebHooks",
+        MANAGE_EMOJIS: "Gerenciar Emojis"
+    }[permission]
 }
